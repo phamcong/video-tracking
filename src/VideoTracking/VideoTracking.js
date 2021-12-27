dd4c963f3528c5ca9344ce643ea44c6b0d49c7cc
Cong cuong PHAM <phamc@ardian.com>
2021-12-27 10:55:53 +0100

    update(video-tracking): add style for boxes

--- src/VideoTracking/VideoTracking.js
+++ src/VideoTracking/VideoTracking.js
@@ -1,31 +1,71 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./VideoTracking.scss";
 import VideoTrackingService from "./VideoTracking.service";
 
-const initPersonBox = {
-  "box": {
-    "bottomRight": {
-      "x": 0,
-      "y": 0
-    },
-    "topLeft": {
-      "x": 0,
-      "y": 0
-    }
-  },
-  "time": 0
+// Class ans style settings
+// according to type of box: person or car
+const BOX_CLASS = {
+  OBJECT_CLASS_PERSON: 'person-box',
+  OBJECT_CLASS_CAR: 'car-box',
 }
 
-function VideoTracking() {
-  const [canvasWidth, setCanvasWidth] = useState(548);
-  const [canvasHeight, setCanvasHeight] = useState(535);
-  const [canvasTop, setCanvasTop] = useState(0);
-  const [canvasLeft, setCanvasLeft] = useState(0);
-  const [currentPersonBox, setCurrentPersonBox] = useState(initPersonBox);
+const BOX_LABEL = {
+  OBJECT_CLASS_PERSON: 'Person',
+  OBJECT_CLASS_CAR: 'Car',
+}
 
+const BOX_LABEL_CLASS = {
+  OBJECT_CLASS_PERSON: 'person-box-label',
+  OBJECT_CLASS_CAR: 'car-box-label'
+}
+
+
+function VideoTracking() {
+  const [currentDisplayBoxes, setCurrentDisplayBoxes] = useState([]);
+  const delta = 100; // a box should be displayed if it's time in [timming - delta, timming + delta];
   const videoTService = new VideoTrackingService();
-  const trackingResult = videoTService.getVideoTrackingData();
-  const personBoxes = trackingResult.personBoxes;
+  const trackingObjects = videoTService.getVideoTrackingData();
+
+  const getCurrentDisplayBoxes = useCallback(
+    (timming) => {
+      return trackingObjects
+        .map((obj) => {
+          const appearances = obj.appearances;
+          const fBoxes = appearances
+            .map((appr, idx) => {
+              const shouldDisplayBoxes = appr.boxes
+                // a box should be displayed if it's time in [timming - delta, timming + delta];
+                .filter((box) => box.time >= timming - delta && box.time <= timming + delta)
+                .map((box) => ({
+                  ...box,
+                  group: idx,
+                  deltaTimming: Math.abs(timming - box.time),
+                }));
+
+              // Display only box with min deltaTimming
+              shouldDisplayBoxes.sort((a, b) => a.deltaTimming >= b.deltaTimming ? 1 : -1);
+              return shouldDisplayBoxes.slice(0, 1);
+            })
+            .reduce((a, b) => [...a, ...b], []);
+
+          // transform boxes with additional prop
+          return fBoxes.map((boxObject) => {
+            const { topLeft, bottomRight } = boxObject.box;
+            return {
+              top: topLeft.y,
+              left: topLeft.x,
+              width: bottomRight.x - topLeft.x,
+              height: bottomRight.y - topLeft.y,
+              id: obj.id,
+              group: boxObject.group,
+              objectClass: obj.objectClass,
+            };
+          });
+        })
+        .reduce((a, b) => [...a, ...b], []);
+    },
+    [trackingObjects]
+  );
 
   useEffect(() => {
     var video = document.getElementById("video");
@@ -35,56 +75,48 @@ function VideoTracking() {
         var $this = this; //cache
         (function loop() {
           if (!$this.paused && !$this.ended) {
+            // update display boxes with current time
             const timming = $this.currentTime;
-            const foundPersonBox = getTrackingBox(personBoxes, timming * 1000);    
-            console.log(foundPersonBox);        
-            if (!foundPersonBox || !boxChanged(currentPersonBox, foundPersonBox)) {
-              setCurrentPersonBox(initPersonBox);
-            }
-            if (foundPersonBox && boxChanged(currentPersonBox, foundPersonBox)) {
-              setCurrentPersonBox(foundPersonBox);
-            }
+            const currDisplayBoxes = getCurrentDisplayBoxes(timming * 1000);
+            setCurrentDisplayBoxes(currDisplayBoxes);
             setTimeout(loop, 1000 / 30); // drawing at 30fps
           }
         })();
       },
       0
     );
-  }, [personBoxes]);
+  }, [trackingObjects, getCurrentDisplayBoxes]);
 
-  useEffect(() => {
-    const box = currentPersonBox.box;
-    const { bottomRight, topLeft } = box;
-    setCanvasTop(topLeft.y);
-    setCanvasLeft(topLeft.x);
-    setCanvasWidth(bottomRight.x - topLeft.x);
-    setCanvasHeight(bottomRight.y - topLeft.y);
-  }, [currentPersonBox]);
-
-  const getTrackingBox = (trackingBoxes, timming) => {
-    const foundBox = trackingBoxes.find(item => item.time >= timming - 100 && item.time <= timming + 100)
-
-    return foundBox;
-  }
-
-  const boxChanged = (prevBox, nextBox) => JSON.stringify(prevBox) !== JSON.stringify(nextBox);
-
-  return (
+  const canvasBoxes = currentDisplayBoxes.map((box, idx) => {
+    const boxLabelStyle = { 
+      top: box.top - 20, 
+      left: box.left
+    }
+    const boxStyle = {
+      top: box.top,
+      left: box.left,
+      width: box.width,
+      height: box.height,
+    };
+    const objectClass = box.objectClass;
+    const boxClass = BOX_CLASS[objectClass];
+    const boxLabel = BOX_LABEL[objectClass];
+    const boxLabelClass = BOX_LABEL_CLASS[objectClass];
+    
+    return (
+      <div>
+        <span className={boxLabelClass} style={boxLabelStyle}>{boxLabel}</span>
+        <canvas className={boxClass} key={idx} style={boxStyle} id="canvas"></canvas>
+      </div>
+    );
+  })
+  return (    
     <div className="video-container">
       <div className="video-wrapper">
         <video controls={true} id="video">
           <source src="videos/porshe.mp4" type="video/mp4" />
         </video>
-        <canvas
-          style={{
-            border: "1px solid red",
-            top: canvasTop,
-            left: canvasLeft,
-            width: canvasWidth + "px",
-            height: canvasHeight + "px",
-          }}
-          id="canvas"
-        ></canvas>
+        {canvasBoxes}
       </div>
     </div>
   );
--- src/VideoTracking/VideoTracking.service.js
+++ src/VideoTracking/VideoTracking.service.js
@@ -1,24 +1,9 @@
-import { porcheObjectDData } from "./data/porche-object-detections";
+import { porcheObjectDData } from "./data/porche-object-detections"
 
 export default class VideoTrackingService {
   getVideoTrackingData() {
-    const porcheDData = porcheObjectDData;
-    const objects = porcheDData?.data?.analysis?.objects || [];
-    const personBoxes = objects
-      .filter((item) => item.objectClass === "OBJECT_CLASS_PERSON")
-      .map((item) => item.appearances)
-      .reduce((a, b) => [...a, ...b], [])
-      .map(item => item.boxes)
-      .reduce((a, b) => [...a, ...b], []);
-    const carBoxes = objects
-      .filter((item) => item.objectClass === "OBJECT_CLASS_CAR")
-      .map((item) => item.appearances)
-      .reduce((a, b) => [...a, ...b], [])
-      .map(item => item.boxes)
-      .reduce((a, b) => [...a, ...b], []);
-    return {
-      personBoxes,
-      carBoxes,
-    };
+    const porcheDData = porcheObjectDData
+    const objects = porcheDData?.data?.analysis?.objects || []
+    return objects
   }
 }
